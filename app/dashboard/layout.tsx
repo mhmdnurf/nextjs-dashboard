e97e0d0f17c84d07a0dbf7b366d0c03cfdcf8549
mhmdnurf <zaldebarenz@gmail.com>
2024-04-29 14:16:29 +0700

    finish learning

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,9 +10,9 @@ export default function Layout({ children }: Layout) {
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
           <SideNav />
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-            {children}
-          </div>
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {children}
         </div>
       </div>
     </>
